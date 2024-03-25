f600d28f844e266dfed56690c480be23ecead9e9
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2024-03-25 22:50:53 +0700

    Fix syntax error on import json component

--- src/app/components/ImportJSON.jsx
+++ src/app/components/ImportJSON.jsx
@@ -149,21 +149,20 @@ export default function ImportJSON() {
                     showValueLabel={true}
                   />
                 )}
-                {(isJsonValid &&
-                 !isImporting) ? (
-                    <div>
-                      <p className="text-sm text-green-500">
-                        JSON is valid and ready to import! Please click the
-                        import button.
-                      </p>
-                    </div>,
-                 ) : null}
+                {isJsonValid && !isImporting ? (
+                  <div>
+                    <p className="text-sm text-green-500">
+                      JSON is valid and ready to import! Please click the import
+                      button.
+                    </p>
+                  </div>
+                ) : null}
                 {isImporting && (
                   <div>
                     <p className="text-sm text-red-500">
                       Do not close the site while the JSON is being imported...
                     </p>
-                  </div>,
+                  </div>
                 )}
               </ModalBody>
               <ModalFooter>
